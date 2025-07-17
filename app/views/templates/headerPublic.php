34c3d58c9e5dc82ada56f2f0fca570f263859e9c
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-17 23:53:57 +0000

    Added public navigation header for logging in and creating an account (for users not signed in)

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -15,3 +15,21 @@ if (isset($_SESSION['auth']) == 1) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/login">COSC 4806</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/create">Create Account</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
