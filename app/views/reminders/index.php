2298f0486bd91756ed0917d93963d60b46a4dde9
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-17 23:20:53 +0000

    Set breadcrumb active item to use session controller variable instead of hardcoded value

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -1,18 +1,17 @@
     <?php require_once 'app/views/templates/header.php' ?>
-<div class="page-header">
+<div class="page-header" id="banner">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="/home">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Reminders</li>
+            <li class="breadcrumb-item active" aria-current="page"><?= ucwords($_SESSION['controller']);?></li>
           </ol>
         </nav>
             </div>
         </div>
-        
-            <div class="row">
+        <div class="row">
             <h1>Reminders</h1>
             <p><a href='reminders/create'>Create a reminder</a></p>
         </div>
