92c8e578bc7606241ceb9909ae857a098c0e035d
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-18 22:07:38 +0000

    Added chart.js library to header.php for creating charts based on our reports

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -14,6 +14,8 @@ if (!isset($_SESSION['auth'])) {
     <title>COSC 4806</title>
     <link rel="stylesheet" href="/app/views/templates/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
