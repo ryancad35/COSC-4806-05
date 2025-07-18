a5106340df21c20334d15a5eef3c84bb2cc01c32
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-18 21:14:32 +0000

    Added functions in reports Controller for viewing all reminders, viewing who has the most reminders, and total logins by username

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -15,3 +15,26 @@ class Reminders extends Controller {
             exit;
         }
     }
+
+    public function index() {
+        $this->view('reports/index');
+    }
+
+    public function view_all_reminders() {
+        $reminder = $this->model('Reminder');
+        $all_reminders = $reminder->get_all_reminders_with_users();
+        $this->view('reports/view_all_reminders', ['reminders' => $all_reminders]);
+    }
+
+    public function most_reminders() {
+        $reminder = $this->model('Reminder');
+        $most_reminders = $reminder->get_most_reminders_by_user();
+        $this->view('reports/most_reminders', ['results' => $most_reminders]);
+    }
+
+    public function login_counts() {
+        $user = $this->model('User');
+        $login_counts = $user->get_login_counts();
+        $this->view('reports/login_counts', ['results' => $login_counts]);
+    }
+}
